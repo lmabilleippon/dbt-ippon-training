eea94e8c2822f9c43b5f0f22b42accb2c81ee802
lmabille@ippon.fr <lmabille@ippon.fr>
2023-10-17 11:55:53 +0000

    add test

--- macros/mockable_source.sql
+++ macros/mockable_source.sql
@@ -1,6 +1,6 @@
-{% macro mockable_source(source_name, table, sample) %}
+{% macro mockable_source(source_name, table, sample_table) %}
     ({%if target.name == "ci" %}
-    {{ ref(sample)}}
+    {{ ref(sample_table)}}
     {% else %}
     {{source(source_name, table)}}
  {% endif %} )
